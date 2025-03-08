49e3d6f48305a84ca1a48098f00facd314c225c8
Utkarsh Mandal <utkarshmandal420@gmail.com>
2025-03-08 18:21:40 +0530

    Add Constitution in Backend

--- prototype/home/urls.py
+++ prototype/home/urls.py
@@ -3,5 +3,7 @@ from django.views.decorators.csrf import csrf_exempt # type: ignore
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('chat', views.chatbot_view, name='chatbot'),
+    path('api/chat/', views.process_chat, name='process_chat')
 ]
